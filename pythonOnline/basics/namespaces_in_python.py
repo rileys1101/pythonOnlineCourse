3e17a0fd56277fd4283a3e8ceb5555f9ec07d99c
rileys1101 <65180029+rileys1101@users.noreply.github.com>
2020-05-13 23:07:43 +0630

    day 2 second commit
    day 2 second commit

--- pythonOnline/basics/namespaces_in_python.py
+++ pythonOnline/basics/namespaces_in_python.py
@@ -7,4 +7,13 @@ def hello():
     print(a) 
 
 hello() # this function is accessable to to global variable a
-print(b) # this line raise an error because the local varible(b) is not accessable from global scope
+#print(b)  this line raise an error because the local varible(b) is not accessable from global scope
+
+
+print('the value of global a is ',a)
+
+def hi():
+    global a
+    a = 12
+hi()
+print('global a value is alter by function hi()',a)
